b2a992e4eca54e2f0ab1147cd9e82c27f7ad264c
Alec Smecher <alec@smecher.bc.ca>
2016-02-16 22:34:21 -0800

    Make code more readable and reapplicable

--- brows.ino
+++ brows.ino
@@ -3,143 +3,183 @@
  * Copyright (c) 2016 by Alec Smecher
  *
  * See LICENSE for licensing details.
- * 
- * Note to me: H-bridge wiring is...
- * 2  5
- * 4  3
+ * See http://www.cassettepunk.com for more information.
  */
 
 #include <IRLib.h>
 
-// Pin configurations
+/**
+ * Pin configurations
+ */
 int motor_pins[2][2] = {
-  {3, 5}, //Motor 1
-  {4, 2} // Motor 2
+  {3, 5}, // Motor 1 outputs to H-bridge on these pins
+  {4, 2}  // Motor 2 outputs to H-bridge on these pins
 };
-IRrecv irr(10);
+IRrecv irr(10); // IR receiver data comes in on this pin
 
-void setup() {
-  Serial.begin(9600);
-  irr.enableIRIn(); // Start the receiver
-  for (int i = 0; i < 2; i++) for (int j=0; j<2; j++) {
-    pinMode(motor_pins[i][j], OUTPUT);
-  }
-}
 
-// Remote control key hashes
-#define VOLD 0xDF604FDC // Big left up calibrate
-#define VOLU 0x20EE60D6 // Big right up calibrate
-#define UP 0x738F2F1A // Both up
-#define DOWN 0x24CA54E0 // Both down
-#define CENTER 0x18313136 // Invert brows
-#define LEFT 0x18B07C3E // Left up, right down
-#define RIGHT 0xAC77E57C // Right up, left down
-#define SRC 0x6F75E25A // Small left up calibrate
-#define AUDIO 0xA7AC0E04  // Small right up calibrate
-#define MUTE 0x6BB9B45A // Small left down calibrate
-#define PGM 0xF3923B3E // Small right down calibrate
-#define OFFHOOK 0xBD892ABE // Big left down calibrate
-#define ONHOOK 0x51B5C084 // Big right down calibrate
-#define BAND 0x57FFB47E // Make movements smaller
-#define FUNC 0x4A7C3080 // Make movements bigger
-#define ONE 0xB203D919 // Waggles
-#define TWO 0x97738AB9 // Waggles
-#define THREE 0x39B169B5 // Waggles
-#define FOUR 0x57AFEA19 // Waggles
-#define FIVE 0xFBDD28D5 // Waggles
-#define SIX 0xAA02A07D // Waggles
-#define SEVEN 0x4C407F79 // Waggles
-#define EIGHT 0xD012B39 // Waggles
-#define NINE 0xC97A06F5 // Waggles
-#define DIRECT 0xC833AE95 // Left waggle selector
-#define CLEAR 0x66236799 // Right waggle selector
-#define ZERO 0x554D315 // Both waggle selector
+// Bitfield for identifying brow states
+const unsigned char BROW_LEFT = 0x01;
+const unsigned char BROW_RIGHT = 0x02;
+const unsigned char BROW_BOTH = BROW_LEFT | BROW_RIGHT;
 
-const unsigned char BROW_LEFT=1;
-const unsigned char BROW_RIGHT=2;
-const unsigned char BROW_BOTH=BROW_LEFT | BROW_RIGHT;
-
+// Constants for movement directions
 const unsigned char DIR_STOP = 0;
 const unsigned char DIR_UP = 1;
 const unsigned char DIR_DOWN = 2;
 
-unsigned char brow_status = 0;
-unsigned char wagglers = BROW_LEFT | BROW_RIGHT;
-unsigned int brow_delay = 1000;
+unsigned char brow_status = 0; // Which brows are up (BROW_... bitfield)
+unsigned char wagglers = BROW_LEFT | BROW_RIGHT; // Which brows are selected for waggling
+unsigned int brow_delay = 1000; // Size of movement
+
+// Amount of time to let the motor run for when calibrating
+const unsigned int CALIBRATE_LARGE = 500;
+const unsigned int CALIBRATE_SMALL = 250;
 
 IRdecodeHash ird;
 
+
+void setup() {
+  Serial.begin(9600); // May be used for debugging when breadboarded on the Arduino
+  irr.enableIRIn(); // Start the receiver
+
+  // Set the pin modes for the H-bridge outputs
+  for (int i = 0; i < 2; i++) for (int j = 0; j < 2; j++) {
+      pinMode(motor_pins[i][j], OUTPUT);
+    }
+}
+
+
 void loop() {
   if (irr.GetResults(&ird)) {
-        unsigned char target_status = brow_status;
-        unsigned char count = 0;
-        ird.decode();
+    // A button was pressed. Do something.
+    unsigned char target_status = brow_status; // Track changes in the desired status
+    unsigned char count = 0; // Waggle counter
+    ird.decode();
 
-	// Pass 1: Handle almost all the buttons
-        switch(ird.hash) {
-          case VOLD: drive(BROW_LEFT, DIR_UP); delay(500); drive(BROW_LEFT, DIR_STOP); break;
-          case VOLU: drive(BROW_RIGHT, DIR_UP); delay(500); drive(BROW_RIGHT, DIR_STOP); break;
-          case SRC: drive(BROW_LEFT, DIR_UP); delay(250); drive(BROW_LEFT, DIR_STOP); break;
-          case AUDIO: drive(BROW_RIGHT, DIR_UP); delay(250); drive(BROW_RIGHT, DIR_STOP); break;
-          case MUTE: drive(BROW_LEFT, DIR_DOWN); delay(250); drive(BROW_LEFT, DIR_STOP); break;
-          case PGM: drive(BROW_RIGHT, DIR_DOWN); delay(250); drive(BROW_RIGHT, DIR_STOP); break;
-          case OFFHOOK: drive(BROW_LEFT, DIR_DOWN); delay(500); drive(BROW_LEFT, DIR_STOP); break;
-          case ONHOOK: drive(BROW_RIGHT, DIR_DOWN); delay(500); drive(BROW_RIGHT, DIR_STOP); break;
-          case UP: target_status = BROW_LEFT | BROW_RIGHT; break;
-          case DOWN: target_status = 0; break;
-          case CENTER: target_status = (BROW_LEFT | BROW_RIGHT)-target_status; break;
-          case LEFT: target_status = BROW_LEFT; break;
-          case RIGHT: target_status = BROW_RIGHT; break;
-          case BAND: target_status=0; break;
-          case FUNC: target_status=0; break;
-          case ONE: target_status=0; count=1; break;
-          case TWO: target_status=0; count=2; break;
-          case THREE: target_status=0; count=3; break;
-          case FOUR: target_status=0; count=4; break;
-          case FIVE: target_status=0; count=5; break;
-          case SIX: target_status=0; count=6; break;
-          case SEVEN: target_status=0; count=7; break;
-          case EIGHT: target_status=0; count=8; break;
-          case NINE: target_status=0; count=9; break;
-          case DIRECT: wagglers = BROW_LEFT; break;
-          case CLEAR: wagglers = BROW_RIGHT; break;
-          case ZERO: wagglers = BROW_LEFT | BROW_RIGHT; break;
-          default: Serial.println(ird.hash, HEX);
-        }
+    // Pass 1: Handle almost all the buttons
+    switch (ird.hash) {
 
-	// Set the brows to the desired state
-        if (brow_status != target_status) {
-          if ((brow_status & BROW_LEFT) && !(target_status & BROW_LEFT)) drive(BROW_LEFT, DIR_DOWN);
-          if ((brow_status & BROW_RIGHT) && !(target_status & BROW_RIGHT)) drive(BROW_RIGHT, DIR_DOWN);
-          if (!(brow_status & BROW_LEFT) && (target_status & BROW_LEFT)) drive(BROW_LEFT, DIR_UP);
-          if (!(brow_status & BROW_RIGHT) && (target_status & BROW_RIGHT)) drive(BROW_RIGHT, DIR_UP);
-          delay(brow_delay);
-          drive(BROW_LEFT | BROW_RIGHT, DIR_STOP);
-          brow_status = target_status;
-        }
+      // Calibration buttons
+      case 0xDF604FDC: // Left brow, large up
+        drive(BROW_LEFT, DIR_UP);
+        delay(CALIBRATE_LARGE);
+        drive(BROW_LEFT, DIR_STOP);
+        break;
+      case 0x20EE60D6: // Right brow, large up
+        drive(BROW_RIGHT, DIR_UP);
+        delay(CALIBRATE_LARGE);
+        drive(BROW_RIGHT, DIR_STOP);
+        break;
+      case 0x6F75E25A: // Left brow, small up
+        drive(BROW_LEFT, DIR_UP);
+        delay(CALIBRATE_SMALL);
+        drive(BROW_LEFT, DIR_STOP);
+        break;
+      case 0xA7AC0E04: // Right brow, small up
+        drive(BROW_RIGHT, DIR_UP);
+        delay(CALIBRATE_SMALL);
+        drive(BROW_RIGHT, DIR_STOP);
+        break;
+      case 0x6BB9B45A: // Left brow, small down
+        drive(BROW_LEFT, DIR_DOWN);
+        delay(CALIBRATE_SMALL);
+        drive(BROW_LEFT, DIR_STOP);
+        break;
+      case 0xF3923B3E: // Right brow, small down
+        drive(BROW_RIGHT, DIR_DOWN);
+        delay(CALIBRATE_SMALL);
+        drive(BROW_RIGHT, DIR_STOP);
+        break;
+      case 0xBD892ABE: // Left brow, big down
+        drive(BROW_LEFT, DIR_DOWN);
+        delay(CALIBRATE_LARGE);
+        drive(BROW_LEFT, DIR_STOP);
+        break;
+      case 0x51B5C084: // Right brow, big down
+        drive(BROW_RIGHT, DIR_DOWN);
+        delay(CALIBRATE_LARGE);
+        drive(BROW_RIGHT, DIR_STOP);
+        break;
 
-	// Pass 2: For a few buttons, we need to do something after setting states
-        switch (ird.hash) {
-          case BAND: if (brow_delay > 100) brow_delay -= 100; break;
-          case FUNC: if (brow_delay < 2000) brow_delay += 100; break;
-        }
+      // Direct selection of brow states
+      case 0x738F2F1A: // Both brows up
+        target_status = BROW_LEFT | BROW_RIGHT;
+        break;
+      case 0x24CA54E0: // Both brows down
+        target_status = 0;
+        break;
+      case 0x18313136: // Invert brows
+        target_status = (BROW_LEFT | BROW_RIGHT) - target_status;
+        break;
+      case 0x18B07C3E: // Left up, right down
+        target_status = BROW_LEFT;
+        break;
+      case 0xAC77E57C: // Right up, left down
+        target_status = BROW_RIGHT;
+        break;
 
-	// If any waggles are needed, perform them.
-        while (count>0) {
-          drive(wagglers, DIR_UP);
-          delay(brow_delay);
-          drive(wagglers, DIR_STOP);
-          drive(wagglers, DIR_DOWN);
-          delay(brow_delay);
-          drive(wagglers, DIR_STOP);
-          count--;
-        }
-        irr.resume();
-      }
+      // Adjust the movement size
+      case 0x57FFB47E: // Make movements smaller
+        target_status = 0;
+        break;
+      case 0x4A7C3080: // Make movements bigger
+        target_status = 0;
+        break;
+
+      // Waggler functions
+      case 0xB203D919: target_status = 0; count = 1; break; // 1 waggle
+      case 0x97738AB9: target_status = 0; count = 2; break; // 2 waggles
+      case 0x39B169B5: target_status = 0; count = 3; break; // 3 waggles
+      case 0x57AFEA19: target_status = 0; count = 4; break; // 4 waggles
+      case 0xFBDD28D5: target_status = 0; count = 5; break; // 5 waggles
+      case 0xAA02A07D: target_status = 0; count = 6; break; // 6 waggles
+      case 0x4C407F79: target_status = 0; count = 7; break; // 7 waggles
+      case 0xD012B39: target_status = 0; count = 8; break; // 8 waggles
+      case 0xC97A06F5: target_status = 0; count = 9; break; // 9 waggles
+      case 0xC833AE95: wagglers = BROW_LEFT; break; // Choose left brow for waggling
+      case 0x66236799: wagglers = BROW_RIGHT; break; // Choose right brow for waggling
+      case 0x554D315: wagglers = BROW_LEFT | BROW_RIGHT; break; // Choose both brows for waggling
+
+      // Dump unknown presses to the serial port
+      default: Serial.println(ird.hash, HEX);
+    }
+
+    // If a brow state change was flagged, set the brows to the desired state
+    if (brow_status != target_status) {
+      if ((brow_status & BROW_LEFT) && !(target_status & BROW_LEFT)) drive(BROW_LEFT, DIR_DOWN);
+      if ((brow_status & BROW_RIGHT) && !(target_status & BROW_RIGHT)) drive(BROW_RIGHT, DIR_DOWN);
+      if (!(brow_status & BROW_LEFT) && (target_status & BROW_LEFT)) drive(BROW_LEFT, DIR_UP);
+      if (!(brow_status & BROW_RIGHT) && (target_status & BROW_RIGHT)) drive(BROW_RIGHT, DIR_UP);
+      delay(brow_delay);
+      drive(BROW_LEFT | BROW_RIGHT, DIR_STOP);
+      brow_status = target_status;
+    }
+
+    // Pass 2: For a few buttons, we need to do something after setting states.
+    switch (ird.hash) {
+      // Make movements smaller
+      case 0x57FFB47E: if (brow_delay > 100) brow_delay -= 100; break;
+      // Make movements bigger
+      case 0x4A7C3080: if (brow_delay < 2000) brow_delay += 100; break;
+    }
+
+    // If any waggles are needed, perform them.
+    while (count > 0) {
+      drive(wagglers, DIR_UP);
+      delay(brow_delay);
+      drive(wagglers, DIR_STOP);
+      drive(wagglers, DIR_DOWN);
+      delay(brow_delay);
+      drive(wagglers, DIR_STOP);
+      count--;
+    }
+    irr.resume();
+  }
 }
 
 /**
- * Drive the brow motors.
+ * Drive the brow motors via the H-bridge.
  * brow: bitfield (see BROW_... constants)
  * direction: One of the DIR_... constants
  */
